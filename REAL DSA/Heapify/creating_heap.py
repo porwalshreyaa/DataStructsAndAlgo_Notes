93ec23cd8da1e39bcedd8b893444e6b6af71b5e4
Shreya Porwal <porwalshreya11111@gmail.com>
2026-01-11 15:48:10 +0530

    comments added

--- REAL DSA/Heapify/creating_heap.py
+++ REAL DSA/Heapify/creating_heap.py
@@ -12,7 +12,7 @@ class MinHeap:
         self._compare_child_and_swap(0)
     def _compare_parent_and_swap(self, index):
         if index>0:
-            p = (index-1)//2
+            p = (index-1)//2 # p = parent
             if self.arr[p] > self.arr[index]:
                 self.arr[p], self.arr[index] = self.arr[index], self.arr[p]
             return self._compare_parent_and_swap(p)
@@ -20,16 +20,16 @@ class MinHeap:
             return
     def _compare_child_and_swap(self, index):
         n = len(self.arr)
-        smallest = index
-        l = 2*index +1
-        r = 2*index +2
+        smallest = index # index of smallest element seen so far
+        l = 2*index +1 # l = left child
+        r = 2*index +2 # r = right child
         if l<n and self.arr[l]<self.arr[smallest]:
             smallest=l
         if r<n and self.arr[r]<self.arr[smallest]:
             smallest=r
         if smallest != index:
-            self.arr[index], self.arr[smallest]=self.arr[smallest], self.arr[index]
-            self._compare_child_and_swap(smallest)
+            self.arr[index], self.arr[smallest]=self.arr[smallest], self.arr[index] # swap the smallest with parent -> min heap
+            self._compare_child_and_swap(smallest) # repeat with the swapped element
     def show(self):
         for i in self.arr:
             print(i)
@@ -51,27 +51,28 @@ class MinHeap:
 # Heapify - parent to children but start from last
 # for minimum
 
-def BottomCompare(arr, ind):
-    n = len(arr)
-    smallest = ind
-    l = ind*2 + 1
-    r = ind*2 + 2
-    if l < n and arr[l]<arr[smallest]:
-        smallest=l
-    if r < n and arr[r]<arr[smallest]:
+def BottomCompare(arr, index): # compare parent node with it's children
+    n = len(arr) # array size
+    smallest = index # index of smallest element seen so far
+    l = index*2 + 1 # l = left child
+    r = index*2 + 2 # r = right child
+    if l < n and arr[l]<arr[smallest]: # child node index present in array  and node left child node smaller than smallest
+        smallest=l # update index of smallest seen
+    if r < n and arr[r]<arr[smallest]: 
         smallest=r
-    if smallest!=ind:
-        arr[smallest], arr[ind]=arr[ind], arr[smallest]
-        BottomCompare(arr, smallest)
+    if smallest!=index:
+        arr[smallest], arr[index]=arr[index], arr[smallest] # swap the smallest with parent -> min heap
+        BottomCompare(arr, smallest) # repeat this with the swapped index - 
+        # why? - incase it is larger than other descendants as well? example - the largest misplaced at top most should go to bottom most
 
-def heapify(arr):
-    n = len(arr)
-    if n<2:
+def heapify(arr): # arr = array
+    n = len(arr) # length of array
+    if n<2: # if there is only 1 element or empty array
         return arr
-    currentIndex = n-1
-    while currentIndex>=0:
-        BottomCompare(arr,currentIndex)
-        currentIndex-=1
+    currentIndex = n-1 # start from last element
+    while currentIndex>=0: 
+        BottomCompare(arr,currentIndex) # heapify/fix the order of indexes - inorder of last filled child node to root node 
+        currentIndex-=1 
     return arr
 
 array2 = [5,2,0,1,9,100,4]
